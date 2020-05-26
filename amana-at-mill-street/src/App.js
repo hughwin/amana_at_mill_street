139e63bb405ae718e89c2a9a1ffc40831e899f02
Hugh Winchester <hugh.winchester@gmail.com>
2020-05-26 10:47:24 +0100

    Basics of app added

---
+++ amana-at-mill-street/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return ( 
+      <div class="top container text-center">
+          <h1 class="title" id="name">Amana at Mill Street</h1>
+      </div>
+  );
+}
+
+export default App;
