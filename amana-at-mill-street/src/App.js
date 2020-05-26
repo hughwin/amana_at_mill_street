651cb3fdb83f74ab3d96ee92927518ab1566e825
Hugh Winchester <hugh.winchester@gmail.com>
2020-05-26 12:25:55 +0100

    Added image roatater

--- amana-at-mill-street/src/App.js
+++ amana-at-mill-street/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import BootstrapCarousel from './components/BootstrapCarousel'
 
 function App() {
   return ( 
+    <div>
       <div class="top container text-center">
           <h1 class="title" id="name">Amana at Mill Street</h1>
       </div>
+<BootstrapCarousel></BootstrapCarousel>
+    </div>
   );
 }
 
