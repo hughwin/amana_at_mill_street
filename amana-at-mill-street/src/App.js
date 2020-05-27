b868d3c1dfb31dbddefc7b7514a3c4e5f63838cb
Hugh Winchester <hugh.winchester@gmail.com>
2020-05-27 15:13:39 +0100

    centred caro

--- amana-at-mill-street/src/App.js
+++ amana-at-mill-street/src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './App.css';
-import BootstrapCarousel from './components/BootstrapCarousel'
+import React from "react";
+import "./App.css";
+import BootstrapCarousel from "./components/BootstrapCarousel";
 
 function App() {
-  return ( 
+  return (
     <div>
       <div class="top container text-center">
-          <h1 class="title" id="name">Amana at Mill Street</h1>
+        <h1 class="title" id="name">
+          Amana at Mill Street
+        </h1>
       </div>
-<BootstrapCarousel></BootstrapCarousel>
+      <div>
+        <h1>Ludlow</h1>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </div>
+      <BootstrapCarousel></BootstrapCarousel>
+      <div>
+        <h1>Availability</h1>
+      </div>
     </div>
   );
 }
--- amana-at-mill-street/src/components/BootstrapCarousel.js
+++ amana-at-mill-street/src/components/BootstrapCarousel.js
@@ -1,35 +1,119 @@
-import Carousel from 'react-bootstrap/Carousel' 
-import React,  { Component } from 'react'
-import livingroom1 from '../static/images/ground-living.jpeg'
-import livingroom2 from '../static/images/ground-living-2.jpeg'
-export class BootStrapCarousel extends Component {
-    render() {
-        return (
-            <div>
-                <div class='container-fluid' >
-                    <div className="row title" style={{ marginBottom : "20px"}}>
-                        </div>
-                        </div>
-                        <div className='container-fluid'>
-                            <Carousel>
-                                <Carousel.Item style = {{'height':"400px"}} >
-                                    <img style={{'height':"400px"}}
-                                    className="d-block w-100"
-                                    src={livingroom1} 
-                                    alt="Living room"/>
-                                    </Carousel.Item>
-                                    <Carousel.Item style = {{'height':"400px"}} >
-                                    <img style={{'height':"400px"}}
-                                    className="d-block w-100"
-                                    src={livingroom2} 
-                                    alt="Living room"/>
-                                    </Carousel.Item>
-                                    </Carousel>
-                                    </div>
-                                    </div>
+import Carousel from "react-bootstrap/Carousel";
+import React, { Component } from "react";
+import livingroom1 from "../static/images/ground-living.jpeg";
+import livingroom2 from "../static/images/ground-living-2.jpeg";
+import upstairsLiving from "../static/images/upstairs-living.jpeg";
+import upstairsLiving2 from "../static/images/upstairs-living-2.jpeg";
+import upstairsLiving3 from "../static/images/upstairs-living-3.jpeg";
+import kitchen from "../static/images/kitchen.jpeg";
+import masterBedroom from "../static/images/master-bedroom.jpeg";
+import bathroom from "../static/images/bathroom.jpeg";
+import bathroom2 from "../static/images/bathroom-2.jpeg";
+import ensuite from "../static/images/ensuite-bathroom.jpeg";
 
-        )
-    }
+export class BootStrapCarousel extends Component {
+  render() {
+    return (
+      <div>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col d-flex justify-content-center">
+              <Carousel>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={livingroom1}
+                    alt="Living room"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={livingroom2}
+                    alt="Living room"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={upstairsLiving}
+                    alt="Upstairs living room 1"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={upstairsLiving}
+                    alt="Upstairs living room 1"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={upstairsLiving2}
+                    alt="Upstairs living room 2"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={upstairsLiving3}
+                    alt="Upstairs living room 3"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={kitchen}
+                    alt="Kitchen"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={masterBedroom}
+                    alt="Master bedroom"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={bathroom}
+                    alt="Bathroom 1"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={bathroom2}
+                    alt="Bathroom 2"
+                  />
+                </Carousel.Item>
+                <Carousel.Item style={{ height: "400px" }}>
+                  <img
+                    style={{ height: "400px" }}
+                    className="d-block w-100"
+                    src={ensuite}
+                    alt="Ensuite"
+                  />
+                </Carousel.Item>
+              </Carousel>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default BootStrapCarousel
+export default BootStrapCarousel;
